56e7fe5dc0c0e01401f4078cc2e4e8d494af79da
Jhonel <jgapazap@unjbg.edu.pe>
2024-07-16 13:01:00 -0500

    Agregue ejercicio 4intercambiofilas

--- 4intercambiofilas.cpp
+++ 4intercambiofilas.cpp
@@ -31,26 +31,8 @@ void mostrarMatricesLadoALado(int m1[4][4], int m2[4][4], int filas, int columna
 }
 
 int main() {
-    int matriz[4][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
-    };
-
-    int matrizAux[4][4];
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            matrizAux[i][j] = matriz[i][j];
-        }
-    }
-
-    int fila1 = 0;
-    int fila2 = 2;
-
-    intercambiarFilas(matrizAux, fila1, fila2);
-    mostrarMatricesLadoALado(matriz, matrizAux, 4, 4);
-    
+	
+	
     return 0;
 }
 
